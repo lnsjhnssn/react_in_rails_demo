346b1b73f3f81f38319922d6cfec8efa437e8927
lnsjhnssn <144560735+lnsjhnssn@users.noreply.github.com>
2024-08-23 13:23:08 +0200

    react router added

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'static_pages#home'
   get 'home' => 'static_pages#home'
   get 'demo' => 'static_pages#demo'
+  get 'demo/*uri' => 'static_pages#demo'
 end
